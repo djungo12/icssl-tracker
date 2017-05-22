6cc8a1b6d178f1f5e787810876b0783d9f382569
Jung Kim <djungo12@gmail.com>
2017-05-22 19:38:13 +0000

    added total, aqua, dshipper, rfh, ws methods in web_scraper_controller

--- app/controllers/web_scraper_controller.rb
+++ app/controllers/web_scraper_controller.rb
@@ -22,6 +22,17 @@ class WebScraperController < ApplicationController
     render template: 'web_scraper/result'
   end
   
+  # get total number of dealers
+  def get_total
+    count = 1
+    @rows.each do |row|
+      count +=1
+    end
+    return count
+  end
+  helper_method :get_total
+  
+  
   # get dealers with aquaculture permits
   def get_aqua
     count = 1
@@ -30,8 +41,45 @@ class WebScraperController < ApplicationController
           count +=1
       end
     end
+    return count
   end
   helper_method :get_aqua
   
+  # get dealers with d-shipper permits
+  def get_dshipper
+    count = 1
+    @rows.each do |row|
+      if row.values[3].to_s.length > 3
+        puts "#{count}. #{row.values[0]}"
+        count +=1
+      end
+    end
+    return count
+  end
+  helper_method :get_dshipper
+  
+  def get_rfh
+      count = 1
+      @rows.each do |row|
+        name = row.values[0]
+        if name.include? "*"
+            count +=1
+        end
+      end
+      return count
+  end
+  helper_method :get_rfh
+  
+  def get_ws
+      count = 1
+      @rows.each do |row|
+        symbol = row.values[4]
+        if symbol.include? "WS"
+            count +=1
+        end
+      end
+      return count
+  end
+  helper_method :get_ws
   
 end
